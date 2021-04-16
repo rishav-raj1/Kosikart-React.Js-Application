56c3a3b4118b25c31f03c08e57e46796b0142277
rishav-raj1 <rishavkumarraj801@gmail.com>
2021-04-16 15:52:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Checkout from "./Checkout";
+import Service from "./Service";
+import Contact from "./Contact";
+import { Redirect, Route, Switch } from "react-router";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/Login" component={Login}/>
+      <Route exact path="/Checkout" component={Checkout}/>
+      <Route exact path="/service" component={Service}/>
+      <Route exact path="/contact" component={Contact}/>
+      <Redirect to="/" />
+
+      
+    </Switch>
+    </>
   );
 }
 
